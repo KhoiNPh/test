5b95ec264c69336bb19775a76acddb4ed4b52071
Nguyen Pham <khoinguyena1@gmail.com>
2023-11-23 23:24:27 +0100

    gpio2 repo1

--- inc/main.h
+++ inc/main.h
@@ -4,7 +4,7 @@
 #define PI 3.14
 #define ADD_BASE 0x12000000
 #define GPIO1	1
-
+#define GPIO2	2
 void function1(int a, int b);
 void function2(float a, float b);
 void function3(unsigned int a, float b);
